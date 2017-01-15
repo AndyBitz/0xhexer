ec88fab02e3c07163e17617a602ca9cee70a8b69
Andy Bitz <artzbitz13+git@gmail.com>
2017-01-15 22:56:58 +0100

    Settings menu fix

--- dev/js/settings.js
+++ dev/js/settings.js
@@ -9,28 +9,50 @@ function Settings() {
 		this.closeMenu.bind(this)
 		);
 
+	window.addEventListener('click', (e)=>{
+
+		if (this.lastChange+10 < new Date().getTime()) {
+			if (this.currentStatus == Settings.OPEN) {
+				this.closeMenu();
+			}
+		}
+
+	});
+
 }
+
+/* STATIC */
+Settings.OPEN	= 1;
+Settings.CLOSE	= 0;
+Settings.TIMER	= 10;
 
 
 Settings.prototype.closeMenu = function() {
 
-	this.currentStatus = 'CLOSE';
 	document.getElementById('settings-menu').className = '';
+	this.changeStatus();
 
 };
 
 
 Settings.prototype.openMenu = function() {
 
-	this.currentStatus = 'OPEN';
 	document.getElementById('settings-menu').className = 'show-menu';
+	this.changeStatus();
+	
 
+};
+
+
+Settings.prototype.changeStatus = function() {
+	this.currentStatus = (this.currentStatus == Settings.OPEN) ? Settings.CLOSE : Settings.OPEN;
+	this.lastChange = new Date().getTime();
 };
 
 
 Settings.prototype.toggle = function() {
 
-	if (this.currentStatus == 'OPEN') {
+	if (this.currentStatus == Settings.OPEN) {
 		this.closeMenu();
 	} else {
 		this.openMenu();
